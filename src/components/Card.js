c033ed29f8a75920ab8074554810e1e960e6c1c5
Kvelail <contactkka@gmail.com>
2021-11-24 19:26:08 +0100

    book output done

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,48 @@ const Card = ({ item }) => {
         <div className="app__card">
             <div className="app__left">
                 <img
-                    src={item.img}
+                    src={
+                        item.volumeInfo.imageLinks
+                            ? item.volumeInfo.imageLinks.thumbnail
+                            : 'img/no-img.jpg'
+                    }
                     alt="book cover"
                     className="app__img"
                 />
             </div>
             <div className="app__right">
-                <h2 className="app__title">{item.title}</h2>
-                <p className="app__author">By: {item.author}</p>
-                <p className="app__publisher">
-                    Published By: {item.publisher}
+                <h2 className="app__title">
+                    {item.volumeInfo.title
+                        ? item.volumeInfo.title
+                        : 'No Title'}
+                </h2>
+                <p className="app__author">
+                    By:{' '}
+                    {item.volumeInfo.authors &&
+                        item.volumeInfo.authors.map((item) => (
+                            <span className="app__span" key={item}>
+                                {item}
+                            </span>
+                        ))}
                 </p>
-                <a href={item.link} className="app__btn">
-                    details
-                </a>
+                <p className="app__publisher">
+                    Published By:{' '}
+                    <span className="app__span">
+                        {item.volumeInfo.publisher
+                            ? item.volumeInfo.publisher
+                            : 'No Publisher'}
+                    </span>
+                </p>
+                {item.volumeInfo.infoLink && (
+                    <a
+                        href={item.volumeInfo.infoLink}
+                        className="app__btn"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        details
+                    </a>
+                )}
             </div>
         </div>
     );
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,19 +2,48 @@ import './App.css';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { useState } from 'react';
 import Card from '../Card';
+import axios from 'axios';
 
 const App = () => {
     const [results, setResults] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const getResults = async () => {
+        const key = 'AIzaSyBckMQCTLmYOP-M2DecRj13OGe9SBfWlSc';
+
+        const base = 'https://www.googleapis.com/books/v1/volumes?';
+        const query = `q=${searchTerm}&key=${key}&maxResults=40`;
+
+        const data = await (await axios.get(base + query)).data.items;
+
+        setResults(data);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        e.target.elements.book.value = '';
+
+        getResults();
+    };
+
+    const handleChange = (e) => {
+        const searchTerm = e.target.value.split(' ').join('');
+
+        setSearchTerm(searchTerm);
+    };
 
     return (
         <div className="app">
             <h1 className="app__heading">book finder</h1>
-            <form className="app__form">
+            <form className="app__form" onSubmit={handleSubmit}>
                 <input
+                    name="book"
                     type="text"
                     className="app__input"
                     placeholder="Enter book name..."
                     autoFocus
+                    onChange={handleChange}
                 />
                 <button className="app__submit">
                     <span className="app__submit-span">
@@ -23,9 +52,10 @@ const App = () => {
                 </button>
             </form>
             <div className="app__output">
-                {results.map((item) => (
-                    <Card key={item.id} item={item} />
-                ))}
+                {results &&
+                    results.map((item) => (
+                        <Card key={item.id} item={item} />
+                    ))}
             </div>
         </div>
     );
